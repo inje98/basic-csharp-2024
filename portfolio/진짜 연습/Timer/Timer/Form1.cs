489117a17334db90516c1e6adb0782c83611e90a
inje98 <kij980723@gmail.com>
2024-04-24 17:28:09 +0900

    개인프젝준비

---
+++ portfolio/진짜 연습/Timer/Timer/Form1.cs
@@ -0,0 +1,45 @@
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 각 화면 초기화
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 타이머 화면
+        }
+
+
+        // 클릭 이벤트핸들러는 도서관리 보면서. 스킬이 있었음.
+        private void StlTimer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StlStopwatch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StlWorldTime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        // 클로징 종료확인 멘트
+
+
+
+
+
+
+    }
+}
